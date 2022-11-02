2584e2e085c64bc41c9e30efffe4e554f94ba0a9
Joaohenrique15 <joao.henrique_15@hotmail.com>
2022-11-02 19:28:30 -0300

    chore: add AccountService

--- src/backoffice/backoffice.module.ts
+++ src/backoffice/backoffice.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CustomerController } from './controllers/customer.controller';
 import { CustomerSchema } from './schemas/customer.schema';
 import { UserSchema } from './schemas/user.schema';
+import { AccountService } from './services/account.service';
 
 @Module({
   imports: [
@@ -19,5 +20,6 @@ import { UserSchema } from './schemas/user.schema';
     BackofficeModule
   ],
   controllers: [CustomerController],
+  providers: [AccountService],
 })
 export class BackofficeModule { }
