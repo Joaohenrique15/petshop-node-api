af3043eb11b113872f14146a349def94626f4a76
Joaohenrique15 <joao.henrique_15@hotmail.com>
2022-11-02 20:37:42 -0300

    chore: add addBillingAddress and addShippingAddress methods

--- src/backoffice/services/customer.service.ts
+++ src/backoffice/services/customer.service.ts
@@ -1,16 +1,35 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import { Address } from "../models/address.model";
 import { Customer } from "../models/customer.models";
 
 @Injectable()
 export class CustomerService {
     constructor(@InjectModel('Customer') private readonly model: Model<Customer>) {
-        
+
     }
 
-    async create(data: Customer) : Promise<Customer> {
+    async create(data: Customer): Promise<Customer> {
         const customer = new this.model(data);
         return await customer.save()
+    }
+
+    async addBillingAddress(document: string, data: Address): Promise<Customer> {
+        const options = { upsert: true };
+        return await this.model.findOneAndUpdate({ document }, {
+            $set: {
+                billingAddress: data
+            }
+        }, options);
+    }
+
+    async addShippingAddress(document: string, data: Address): Promise<Customer> {
+        const options = { upsert: true };
+        return await this.model.findOneAndUpdate({ document }, {
+            $set: {
+                shippingAddress: data
+            }
+        }, options);
     }
 }
