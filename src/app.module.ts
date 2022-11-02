0c1b17ec8e2a78e11f89798039c11b97aefb5b94
Joaohenrique15 <joao.henrique_15@hotmail.com>
2022-11-02 16:12:21 -0300

    chore: add mongo config and schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BackofficeModule } from './backoffice/backoffice.module';
 
 @Module({
-  imports: [BackofficeModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    BackofficeModule
+  ],
   controllers: [],
   providers: [],
 })
