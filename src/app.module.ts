65694555ac663508f4bae3d33889f444a6ac7409
Joaohenrique15 <joao.henrique_15@hotmail.com>
2022-11-03 09:23:00 -0300

    chore: add store module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BackofficeModule } from './modules/backoffice/backoffice.module';
+import { StoreModule } from './modules/store/store.module';
 
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/petshopApi'),
-    BackofficeModule
+    BackofficeModule,
+    StoreModule
   ],
   controllers: [],
   providers: [],
