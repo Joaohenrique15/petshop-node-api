19c31c9df368cd055fd9580b36804822afffb022
Joaohenrique15 <joao.henrique_15@hotmail.com>
2022-11-02 23:15:37 -0300

    refactor: move petsController to a single file

--- src/modules/backoffice/backoffice.module.ts
+++ src/modules/backoffice/backoffice.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AddressController } from './controllers/address.controller';
 import { CustomerController } from './controllers/customer.controller';
+import { PetController } from './controllers/pet.controller';
 import { CustomerSchema } from './schemas/customer.schema';
 import { UserSchema } from './schemas/user.schema';
 import { AccountService } from './services/account.service';
@@ -23,7 +24,7 @@ import { PetService } from './services/pet.service';
     ]),
     BackofficeModule
   ],
-  controllers: [CustomerController, AddressController],
+  controllers: [CustomerController, AddressController, PetController],
   providers: [AccountService, CustomerService, AddressService, PetService],
 })
 export class BackofficeModule { }
---
+++ src/modules/backoffice/controllers/pet.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Post, Put, Param, Body, UseInterceptors, HttpException, HttpStatus } from '@nestjs/common';
+import { ValidatorInterceptor } from 'src/interceptors/validator.interceptor';
+import { CreatePetContract } from '../contracts/customer/create-pet.contract';
+import { Pet } from '../models/pet.model';
+import { Result } from '../models/result.model';
+import { PetService } from '../services/pet.service';
+
+@Controller('v1/pets')
+export class PetController {
+    constructor(private readonly petService: PetService) {
+    }
+
+    @Post(':document/pets')
+    @UseInterceptors(new ValidatorInterceptor(new CreatePetContract))
+    async createPet(@Param('document') document, @Body() model: Pet) {
+        try {
+            const res = await this.petService.createPet(document, model);
+            return new Result('Pet inserido com sucesso', true, model, null)
+
+        } catch (error) {
+            throw new HttpException(new Result('Não foi possível adicionar seu pet', false, null, error), HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @Put(':document/:id/pets')
+    @UseInterceptors(new ValidatorInterceptor(new CreatePetContract))
+    async updatePet(@Param('document') document, @Param('id') id, @Body() model: Pet) {
+        try {
+            const res = await this.petService.updatePet(document, id, model);
+            return new Result('Pet atualizado com sucesso', true, model, null)
+
+        } catch (error) {
+            throw new HttpException(new Result('Não foi possível atualizar seu pet', false, null, error), HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+}
+
