689ee9f5cf970fb38c4cbff6b1b28830f87e382e
Joaohenrique15 <joao.henrique_15@hotmail.com>
2022-11-03 17:14:04 -0300

    chore: TypeOrm to mySQL and configuration in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BackofficeModule } from './modules/backoffice/backoffice.module';
 import { StoreModule } from './modules/store/store.module';
 
@@ -7,10 +8,20 @@ import { StoreModule } from './modules/store/store.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/petshopApi'),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Password',
+      database: 'petshopApi',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     BackofficeModule,
     StoreModule
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
