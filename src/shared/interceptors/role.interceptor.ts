77d1b2bc626c755ca377fa974b4f319faa87be6e
Joaohenrique15 <joao.henrique_15@hotmail.com>
2022-11-06 14:24:36 -0300

    chore: add RoleInterceptor

---
+++ src/shared/interceptors/role.interceptor.ts
@@ -0,0 +1,30 @@
+import { Injectable, NestInterceptor, ExecutionContext, HttpException, HttpStatus, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Result } from 'src/modules/backoffice/models/result.model';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+
+
+@Injectable()
+export class RoleInterceptor implements NestInterceptor {
+    constructor(public roles: string[]) {
+
+    }
+
+    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+        const payload: JwtPayload = context.switchToHttp().getRequest().user;
+        console.log(payload);
+
+        let hasRole = false;
+        payload.roles.forEach((role) => {
+            if (this.roles.includes(role))
+                hasRole = true;
+        });
+
+        if (!hasRole) {
+            throw new HttpException(
+                new Result('Acesso não autorizado', false, null, null), HttpStatus.UNAUTHORIZED);
+        }
+
+        return next.handle();
+    }
+}
