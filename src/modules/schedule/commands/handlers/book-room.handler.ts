3075483325810ec0dc52a8667746d4a64072d897
Joaohenrique15 <joao.henrique_15@hotmail.com>
2022-11-21 21:31:56 -0300

    feat: implemeting BookRoomHandler

--- src/modules/schedule/commands/handlers/book-room.handler.ts
+++ src/modules/schedule/commands/handlers/book-room.handler.ts
@@ -1,14 +1,22 @@
-import { ICommandHandler } from "@nestjs/cqrs";
+import { HttpException, HttpStatus } from "@nestjs/common";
+import { EventPublisher, ICommandHandler } from "@nestjs/cqrs";
 import { RoomRepository } from "../../repositories/room.repository";
 import { BookRoomCommand } from "../book-room.command";
 
 export class BookRoomHandler implements ICommandHandler<BookRoomCommand> {
-    constructor(private readonly roomRepository: RoomRepository) { }
+    constructor(private readonly roomRepository: RoomRepository, private readonly publisher: EventPublisher,) { }
 
-    async execute(command: BookRoomCommand): Promise<any> {
-        console.log('BookRoomHandler:execute - Executando o comando ...')
-        const room = await this.roomRepository.findOneById(command.roomId)
-        room.book(command.customerId)
-        //commit()
+    async execute(command: BookRoomCommand) {
+        const room = this.publisher.mergeObjectContext(
+            await this.roomRepository.checkAvailability(command.roomId, command.date),
+        );
+
+        if (room) {
+            room.book(command.customerId, command.date);
+            await this.roomRepository.book(room);
+            return;
+        }
+
+        throw new HttpException("Sala não disponível", HttpStatus.BAD_REQUEST);
     }
 }
