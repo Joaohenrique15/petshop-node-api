0efb0444c7c10334a2626a4821251d30ebb5be35
Joaohenrique15 <joao.henrique_15@hotmail.com>
2022-11-21 21:03:04 -0300

    chore: add dependencies

--- src/modules/schedule/schedule.module.ts
+++ src/modules/schedule/schedule.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { CommandHandlers } from './commands/handlers';
+import { ScheduleController } from './controllers/schedule.controller';
+import { RoomRepository } from './repositories/room.repository';
+import { RoomBookService } from './service/room-book.service';
 
-@Module({})
+@Module({
+    imports: [CqrsModule],
+    controllers: [ScheduleController],
+    providers: [
+        RoomBookService,
+        RoomRepository,
+        ...CommandHandlers
+    ],
+})
 export class ScheduleModule {}
